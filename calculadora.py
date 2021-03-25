ce27c54a1fc6fc901c58a389d207093c4f2a7349
AlejandroPuentes <brianpc200@gmail.com>
2021-03-25 10:47:52 -0500

    agregada funciones

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,20 @@
 class Calculadora:
     def _init_(self):
-        self valor_1 = 0
-        self valor_2 = 0
-        self resultado =0
+        self.valor_1 = 0
+        self.valor_2 = 0
+        self.resultado =0
 
     def suma (self):
         self.resultado = self.valor_1 + self.valor_2
+    
+    def resta (self):
+        self.resultado = self.valor_1 - self.valor_2
+    
+    def multiplicacion (self):
+        self.resultado = self.valor_1 * self.valor_2
+    
+    def division (self):
+        if(self.valor_2!=0):
+            self.resultado = self.valor_1 / self.valor_2
+        else:
+            print("no se puede hacer division por cero")
--- testCalcula.py
+++ testCalcula.py
@@ -2,8 +2,11 @@ from calculadora import Calculadora
 
 c=Calculadora()
 
-c.valor_1=2
-c.valor_2=9
-c.suma()
+c.valor_1 =  2
+c.valor_2 = 9
 
+c.resta()
+#c.suma()
+#c.multiplicacion()
+#c.division()
 print(c.resultado)
